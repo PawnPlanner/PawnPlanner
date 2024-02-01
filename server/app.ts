aa0d40d60b41cd58dd51338f8818a0271ec03700
ekane02 <70538006+ekane02@users.noreply.github.com>
2024-02-01 18:09:37 -0500

    create server structure

--- server/app.ts
+++ server/app.ts
@@ -2,10 +2,23 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import express from 'express';
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
+import logger from "./middlewares/logger";
 
 const app = express();
+
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger);
+
 
 const port = process.env.PORT || "8080";
 
