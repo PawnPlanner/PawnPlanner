e947f13a1d1a124d7f2e68dc9506a28da4535755
srh919 <89631009+srh919@users.noreply.github.com>
2024-02-22 14:18:31 -0500

    Update router.tsx

--- client/src/routes/router.tsx
+++ client/src/routes/router.tsx
@@ -7,6 +7,7 @@ import HomePage from "../pages/home-page";
 import CreateTournament from "../pages/CreateTournament";
 import TournamentInfo from "../pages/TournamentInfo"
 import SettingsPage from "../pages/settings-page";
+import Round from "../pages/Round";
 
 const Router = () => {
     let element = useRoutes([
@@ -33,6 +34,10 @@ const Router = () => {
         {
             path: "Settings",
             element: <SettingsPage />
+        },
+        {
+            path: "/Round",
+            element: <Round/>,
         },
     ]);
     return element;
